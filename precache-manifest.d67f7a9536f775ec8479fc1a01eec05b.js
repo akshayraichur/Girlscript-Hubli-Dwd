7ea9be927155c5fb9d56602fb4162197e7d97872
Akshay Raichur <lordakshay99@gmail.com>
2020-09-07 13:07:07 +0530

    Updates

---
+++ precache-manifest.d67f7a9536f775ec8479fc1a01eec05b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a3252b849db799251b7635835ef524",
+    "url": "/Girlscript-Hubli-Dwd/index.html"
+  },
+  {
+    "revision": "b3ec907f95296215c082",
+    "url": "/Girlscript-Hubli-Dwd/static/css/2.808eb894.chunk.css"
+  },
+  {
+    "revision": "7c42624d76d7ebd2e44f",
+    "url": "/Girlscript-Hubli-Dwd/static/css/main.61c122d4.chunk.css"
+  },
+  {
+    "revision": "b3ec907f95296215c082",
+    "url": "/Girlscript-Hubli-Dwd/static/js/2.d8fafa60.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Girlscript-Hubli-Dwd/static/js/2.d8fafa60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c42624d76d7ebd2e44f",
+    "url": "/Girlscript-Hubli-Dwd/static/js/main.ad84a6a2.chunk.js"
+  },
+  {
+    "revision": "776c7ce36abb93fc3449",
+    "url": "/Girlscript-Hubli-Dwd/static/js/runtime-main.d3726f84.js"
+  },
+  {
+    "revision": "e0aab40f55230c96a5e5fc58fcb870e4",
+    "url": "/Girlscript-Hubli-Dwd/static/media/AkshataKulkarni.e0aab40f.jpg"
+  },
+  {
+    "revision": "ae76294d1bc44f710305f539d2e8bfa3",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Akshay.ae76294d.jpeg"
+  },
+  {
+    "revision": "fd763fdbce14fc06f1f4f32dbfe6bcdc",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Mahalxmi.fd763fdb.jpg"
+  },
+  {
+    "revision": "3aceee5c8c74617f24e4394e13697621",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Nerd-amico.3aceee5c.png"
+  },
+  {
+    "revision": "279838b1f1e8ff69df248416707e35fc",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Nikhataara.279838b1.jpeg"
+  },
+  {
+    "revision": "e0bbeadf17f04e84e228b004c3ee03a8",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Nischal.e0bbeadf.jpeg"
+  },
+  {
+    "revision": "c763a04a325b339239f7950c6680f4ca",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Programming.c763a04a.gif"
+  },
+  {
+    "revision": "ca6dcef597f826b1d20f082a6f803c69",
+    "url": "/Girlscript-Hubli-Dwd/static/media/Raksha.ca6dcef5.jpg"
+  },
+  {
+    "revision": "25afb9661c65c7470173ce1e7045603d",
+    "url": "/Girlscript-Hubli-Dwd/static/media/afzal.25afb966.jpg"
+  },
+  {
+    "revision": "89c85b863c42864ff2f55d9ab529102a",
+    "url": "/Girlscript-Hubli-Dwd/static/media/bhumika.89c85b86.jpeg"
+  },
+  {
+    "revision": "cd9585026db3aa4a2055e726258a170a",
+    "url": "/Girlscript-Hubli-Dwd/static/media/gagan.cd958502.jpg"
+  },
+  {
+    "revision": "50bb4d61e84dbccf12378426c5d68fd6",
+    "url": "/Girlscript-Hubli-Dwd/static/media/gs-hd2.50bb4d61.png"
+  },
+  {
+    "revision": "364c319e93936a52a852dcd678a7dc31",
+    "url": "/Girlscript-Hubli-Dwd/static/media/karan.364c319e.jpg"
+  },
+  {
+    "revision": "1943d5e29d6f4c5751483d35a6c2c691",
+    "url": "/Girlscript-Hubli-Dwd/static/media/kishan.1943d5e2.jpeg"
+  },
+  {
+    "revision": "4549fde8d1716b7d4d55c91385265f8d",
+    "url": "/Girlscript-Hubli-Dwd/static/media/kushiYagnik.4549fde8.jpg"
+  },
+  {
+    "revision": "81b377e50d39cd7de30b3a5ea00c4844",
+    "url": "/Girlscript-Hubli-Dwd/static/media/mainbg.81b377e5.svg"
+  },
+  {
+    "revision": "69825287a3aec3a41fd056fa33410849",
+    "url": "/Girlscript-Hubli-Dwd/static/media/memphisbg.69825287.svg"
+  },
+  {
+    "revision": "a1b4a84ec2da6d256cfa00ed33d36ba9",
+    "url": "/Girlscript-Hubli-Dwd/static/media/muskan.a1b4a84e.jpeg"
+  },
+  {
+    "revision": "e64d9a63da2c469371e9776185ae34b6",
+    "url": "/Girlscript-Hubli-Dwd/static/media/rashmi-.e64d9a63.jpg"
+  },
+  {
+    "revision": "7140eac6e913d610cc089cc17ed51607",
+    "url": "/Girlscript-Hubli-Dwd/static/media/sahana.7140eac6.jpg"
+  },
+  {
+    "revision": "7ad5d91684c55b0c3b07a1da945aaf26",
+    "url": "/Girlscript-Hubli-Dwd/static/media/saloniSingh.7ad5d916.jpg"
+  },
+  {
+    "revision": "959b2862bf3d692fe66a9cd49085e33a",
+    "url": "/Girlscript-Hubli-Dwd/static/media/sampreetha-ch.959b2862.jpg"
+  },
+  {
+    "revision": "4b55ac9fd219283ebbcfe0c7df09b70d",
+    "url": "/Girlscript-Hubli-Dwd/static/media/shwetashetti.4b55ac9f.jpg"
+  },
+  {
+    "revision": "cce72e971b6f783b883392827b6cb675",
+    "url": "/Girlscript-Hubli-Dwd/static/media/sohailMalik.cce72e97.jpg"
+  },
+  {
+    "revision": "3eb56ea0ad82cf215c594a46ca6f9ad3",
+    "url": "/Girlscript-Hubli-Dwd/static/media/suhani.3eb56ea0.jpg"
+  },
+  {
+    "revision": "b1298c4c24c7c69b0a409802a776f444",
+    "url": "/Girlscript-Hubli-Dwd/static/media/supriya.b1298c4c.jpeg"
+  },
+  {
+    "revision": "e4ff261bb0a79a759db55a99953de2c5",
+    "url": "/Girlscript-Hubli-Dwd/static/media/versioncontrol.e4ff261b.gif"
+  }
+]);
